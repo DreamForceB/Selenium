793cf7146c9f7c45e645ebd201cac7109dd1f613
DreamForceB <ramkee7472@gmail.com>
2024-06-20 17:34:24 +0530

    DemoScreenshot class added in Screenshot package

---
+++ src/screenshots/DemoScreenshot.java
@@ -0,0 +1,20 @@
+package screenshots;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class DemoScreenshot {
+	
+  @Test
+  public void techlearn() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
